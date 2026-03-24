724315aa7bf4aeb47f947ce7a4aa76c961d833d4
IsaacP <isp171066@gmail.com>
2026-03-24 15:49:01 +0200

    feat use routes on server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import router from "../src/routes/routes";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -8,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-
+app.use(router);
 // Basic route
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript + Express!");
