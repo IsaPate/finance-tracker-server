abe2da6435cf49bb28f79229a7188f441df160ee
IsaacP <isp171066@gmail.com>
2026-03-24 11:55:31 +0200

    feat transactions queries

---
+++ src/models/transaction.server.ts
@@ -0,0 +1,63 @@
+import { $Enums } from "@prisma/client";
+import { prisma as db } from "../lib/prisma_client";
+
+export const createTransaction = async (
+  userId: number,
+  title: string,
+  amount: number,
+  transactionType: $Enums.TransactionType,
+  createdAt: Date,
+  categoryId?: number
+) => {
+  return await db.transaction.create({
+    data: {
+      title,
+      amount,
+      type: transactionType,
+      createdAt,
+      userId,
+      categoryId,
+    },
+  });
+};
+
+export const getTransactionsByUserId = async (userId: number) => {
+  return db.transaction.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+};
+
+export const getTransactionById = async (transactionId: number) => {
+  return db.transaction.findUnique({
+    where: {
+      id: transactionId,
+    },
+  });
+};
+
+export const udpateTransaction = async (
+  userId: number,
+  transactionId: number,
+  title: string,
+  amount: number,
+  createdAt: Date,
+  transactionType: $Enums.TransactionType
+) => {
+  return db.transaction.updateMany({
+    where: {
+      id: transactionId,
+      userId,
+    },
+    data: {
+      title,
+      amount,
+      createdAt,
+      type: transactionType,
+    },
+  });
+};
