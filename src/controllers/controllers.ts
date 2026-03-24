c715ca6952a69a78d4aeb246f5957866b6d345c0
IsaacP <isp171066@gmail.com>
2026-03-24 16:12:40 +0200

    feat controller functions

---
+++ src/controllers/controllers.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+
+export async function getUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {}
+
+export async function createUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {}
+export async function getTransaction(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {}
+export async function createTransaction(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {}
+export async function getTransactions(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {}
+export async function createCategory(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {}
+export async function getCategory(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {}
