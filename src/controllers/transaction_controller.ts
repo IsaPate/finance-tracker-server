7e53b85cf9ee3623056d375a270e903ecc1b6020
IsaacP <isp171066@gmail.com>
2026-03-25 13:18:24 +0200

    feat separate controllers according to schema tables

--- src/controllers/transaction_controller.ts
+++ src/controllers/transaction_controller.ts
@@ -1,61 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { createUser, getUserById } from "../models/user.server";
+
 import {
   createTransaction,
   getTransactionByUserIdAndTransactionId,
   getTransactionsByUserId,
 } from "../models/transaction.server";
-
-export async function getUserHandler(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const userId = req.params.userId;
-  const user = await getUserById(Number(userId));
-  if (!user) {
-    return res.status(404).json({
-      id: "",
-      name: "",
-      success: false,
-    });
-  }
-  return res.status(200).json({
-    id: user.id,
-    name: user.name,
-    success: true,
-  });
-}
-
-export async function createUserHandler(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  const { name } = req.body;
-
-  if (typeof name !== "string") {
-    return res.status(400).json({
-      id: "",
-      name: "",
-      success: false,
-    });
-  }
-
-  const created = await createUser(name);
-  if (!created) {
-    return res.status(500).json({
-      id: "",
-      name: "",
-      success: false,
-    });
-  }
-  return res.status(201).json({
-    id: created.id,
-    name: created.name,
-    success: true,
-  });
-}
 export async function getUserTransactionHandler(
   req: Request,
   res: Response,
@@ -172,13 +122,3 @@ export async function getUserTransactionsHandler(
     success: true,
   });
 }
-export async function createCategory(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {}
-export async function getCategory(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {}
