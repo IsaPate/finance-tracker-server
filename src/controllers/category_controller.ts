394cccc8b7ead6831a0303e2f169fe775e80704c
IsaacP <isp171066@gmail.com>
2026-03-28 19:44:38 +0200

    feat new requests for category

--- src/controllers/category_controller.ts
+++ src/controllers/category_controller.ts
@@ -1,12 +1,79 @@
 import { Request, Response, NextFunction } from "express";
+import {
+  createCategory,
+  deleteCategory,
+  getCategoryByTitle,
+  updateCategory,
+} from "../models/category.server";
 
-export async function createCategory(
+// REQUESTS FOR SINGLE CATEGORY
+export async function createCategoryHandler(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
-export async function getCategory(
+) {
+  const { title } = req.body;
+  await createCategory(title);
+  return res.status(201).json({
+    message: "Category created.",
+    success: true,
+  });
+}
+export async function getCategoryHandler(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+) {
+  const { title } = req.params;
+  if (typeof title !== "string") {
+    return res.status(400).json({
+      message: "Invalid title parameter.",
+      success: false,
+    });
+  }
+  const category = await getCategoryByTitle(title);
+  return res.status(200).json({
+    message: "Category found.",
+    data: category,
+    success: true,
+  });
+}
+
+export async function deleteCategoryHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { title } = req.params;
+  if (typeof title !== "string") {
+    return res.status(400).json({
+      message: "Invalid title parameter.",
+      success: false,
+    });
+  }
+  await deleteCategory(title);
+  return res.status(201).json({
+    message: "Category deleted.",
+    success: true,
+  });
+}
+
+export async function editCategoryHandler(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { title } = req.params;
+  const { newTitle } = req.body;
+  if (typeof title !== "string") {
+    return res.status(400).json({
+      message: "Invalid title parameter.",
+      success: false,
+    });
+  }
+  await updateCategory(title, newTitle);
+  return res.status(201).json({
+    message: "Category edited.",
+    success: true,
+  });
+}
