821b9e78958aeb3923b67cefe0c87526fb31fc75
IsaPate <isaakpaterakis@gmail.com>
2026-07-10 10:36:13 +0300

    feat: logout route

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 import { loginUser, registerUser } from "../controllers/auth_controller";
 import { asyncHandler } from "../middlewares/handlers";
-import { refreshTokenHandler } from "../middlewares/auth";
+import { refreshTokenHandler, logoutHandler } from "../middlewares/auth";
 
 const authRouter = Router();
 authRouter.post("/register", asyncHandler(registerUser));
 authRouter.post("/login", asyncHandler(loginUser));
 authRouter.post("/refresh", asyncHandler(refreshTokenHandler));
+authRouter.post("/logout", asyncHandler(logoutHandler));
 export { authRouter };
