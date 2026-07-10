296378e3278d3ec3f67f8a22cc3c1b247e654a20
IsaPate <isaakpaterakis@gmail.com>
2026-07-10 20:50:18 +0300

    fix: admin rights on searching user by id

--- src/routes/user_routes.ts
+++ src/routes/user_routes.ts
@@ -5,13 +5,14 @@ import {
   getAllUserHandler,
 } from "../controllers/user_controller";
 import { asyncHandler } from "../middlewares/handlers";
-import { verifyTokenMiddleware } from "../middlewares/auth";
+import { isAdmin, verifyTokenMiddleware } from "../middlewares/auth";
 
 const userRouter = Router();
 
 userRouter.get(
   "/users/:userId",
   verifyTokenMiddleware,
+  isAdmin,
   asyncHandler(getUserHandler)
 );
 
