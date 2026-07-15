88317241f7def31ca164bd0f1ffd4e3bd7fb11e0
IsaPate <isaakpaterakis@gmail.com>
2026-07-15 16:58:44 +0300

    feat: transation schema

---
+++ src/schemas/transaction.schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const transactionSchema = z.object({
+  title: z.string().min(1, "Title is required").max(100),
+  amount: z
+    .number()
+    .positive("Amount must be positive number.")
+    .multipleOf(0.01, "Max 2 decimal places"),
+  type: z.enum(["INCOME", "EXPENSE"]),
+  createdAt: z.string().datetime(),
+  userId: z.number(),
+  category: z.string().min(1).max(100).optional(),
+});
