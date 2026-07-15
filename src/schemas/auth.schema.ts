dbaffb8d2acd51c69d01fb32a7265d37fed78fce
IsaPate <isaakpaterakis@gmail.com>
2026-07-15 16:59:27 +0300

    fix: auth schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 export const registrationSchema = z.object({
-  name: z.string(),
+  name: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(2, "Name must be at least 50 characters")
+    .regex(/^[\p{L}\s'-]+$/u, "Name must contain only letters"),
   email: z.string().email(),
   password: z.string().min(8),
 });
