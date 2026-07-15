729448647dc8b8d039989e761dee9c4f711f3636
IsaPate <isaakpaterakis@gmail.com>
2026-07-15 16:59:11 +0300

    feat: validation middleware

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -3,10 +3,20 @@ import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/auth_controller";
 import { asyncHandler } from "../middlewares/handlers";
 import { refreshTokenHandler, logoutHandler } from "../middlewares/auth";
+import { validationMiddleware } from "../middlewares/validate";
+import { loginSchema, registrationSchema } from "../schemas/auth.schema";
 
 const authRouter = Router();
-authRouter.post("/register", asyncHandler(registerUser));
-authRouter.post("/login", asyncHandler(loginUser));
+authRouter.post(
+  "/register",
+  validationMiddleware(registrationSchema),
+  asyncHandler(registerUser)
+);
+authRouter.post(
+  "/login",
+  validationMiddleware(loginSchema),
+  asyncHandler(loginUser)
+);
 authRouter.post("/refresh", asyncHandler(refreshTokenHandler));
 authRouter.post("/logout", asyncHandler(logoutHandler));
 export { authRouter };
