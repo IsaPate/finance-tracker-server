69ee03f5ad282ca3d549ae7d3d08f1a5d0c13212
IsaPate <isaakpaterakis@gmail.com>
2026-07-16 11:56:09 +0300

    fix: updated configs

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
+  },
 };
