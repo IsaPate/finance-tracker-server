21bbe83223e8f7b6fc00035e618e95acdf048593
IsaPate <isaakpaterakis@gmail.com>
2026-07-17 23:00:41 +0300

    feat: variables for nodemailer exported from env

--- src/lib/env.export.ts
+++ src/lib/env.export.ts
@@ -3,6 +3,13 @@ dotenv.config();
 
 const config = {
   clientUrl: process.env.CLIENT_URL,
+  nodemailer: {
+    host: process.env.NODE_MAILER_HOST,
+    username: process.env.NODE_MAILER_USERNAME,
+    password: process.env.NODE_MAILER_PASSWORD,
+    auth: process.env.NODE_MAILER_AUTH,
+    port: process.env.NODE_MAILER_PORT,
+  },
 };
 
 export default config;
