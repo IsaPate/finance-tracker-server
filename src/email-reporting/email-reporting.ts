c3059ccaeffc28356010f63bbe90e97a8c2d8f56
IsaPate <isaakpaterakis@gmail.com>
2026-07-21 16:32:00 +0300

    feat: function for email reporting

---
+++ src/email-reporting/email-reporting.ts
@@ -0,0 +1,44 @@
+import { normalizeStatistics } from "../controllers/helper";
+import { EmailReportingService } from "../lib/mailer";
+import { getTransactionSumByTypeAndEmail } from "../models/transaction.server";
+import { groupedTransactionsByTypeAndCategoryId } from "../models/transaction.server";
+
+export async function emailReporting(email: string, monthRange: Date) {
+  const groupedTransactions = await groupedTransactionsByTypeAndCategoryId(
+    email
+  );
+
+  const totalIncome = groupedTransactions
+    .filter((g) => g.type === "INCOME")
+    .reduce((acc, income) => {
+      if (income._sum.amount) {
+        acc = acc + income._sum.amount;
+      } else {
+        acc = acc + 0;
+      }
+      return acc;
+    }, 0);
+
+  const totalExpense = groupedTransactions
+    .filter((g) => g.type === "EXPENSE")
+    .reduce((acc, income) => {
+      if (income._sum.amount) {
+        acc = acc + income._sum.amount;
+      } else {
+        acc = acc + 0;
+      }
+      return acc;
+    }, 0);
+  const emailReportingService = new EmailReportingService(
+    email,
+    groupedTransactions.map((g) => {
+      return {
+        amount: g._sum.amount,
+        ...g,
+      };
+    }),
+    totalIncome,
+    totalExpense
+  );
+  await emailReportingService.emailSender();
+}
