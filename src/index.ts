9d68f1b33704e5b7484d9a54258668b6e2d9b653
IsaPate <isaakpaterakis@gmail.com>
2026-08-03 12:01:53 +0300

    fix: move code to email module

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,10 @@
 import cron from "node-cron";
 import { app } from "./app";
 import { logger } from "./lib/logger";
-import { getUsersWithEnabledReporting } from "./models/user.server";
-import PQueue from "p-queue";
-import { emailReporting } from "./email-reporting/email-reporting";
+import { scanForEmailReporting } from "./email-reporting-modules/email-reporting";
 
 const port = process.env.PORT || 3000;
 
-const pQueue = new PQueue({ concurrency: 2 });
-
-async function scanForEmailReporting() {
-  const now = new Date();
-  const start = new Date(now.getFullYear(), now.getMonth() - 1, 1);
-  const end = new Date(now.getFullYear(), now.getMonth(), 1);
-
-  const users = await getUsersWithEnabledReporting();
-  for (const user of users) {
-    pQueue.add(async () => {
-      try {
-        await emailReporting(user.email, {
-          start,
-          end,
-        });
-      } catch (err) {
-        logger.error(
-          { email: user.email, err },
-          "failed to send monthly report"
-        );
-      }
-    });
-  }
-}
 cron.schedule("30 10 1 * *", async () => {
   try {
     await scanForEmailReporting();
