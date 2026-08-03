64ee5c0be49e552c5560703985abcc31c67dee6d
IsaPate <isaakpaterakis@gmail.com>
2026-08-03 12:19:20 +0300

    feat: routes for recurring charges

--- src/routes/recurring_charge_routes.ts
+++ src/routes/recurring_charge_routes.ts
@@ -2,4 +2,9 @@ import { Router } from "express";
 
 const recurringChargesRouter = Router();
 
+recurringChargesRouter.get("/users/:userId/recurring-charges");
+recurringChargesRouter.post("/users/:userId/recurring-charges");
+recurringChargesRouter.put("/users/:userId/recurring-charges");
+recurringChargesRouter.delete("/users/:userId/recurring-charges");
+
 export default recurringChargesRouter;
