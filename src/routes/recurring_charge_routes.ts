2a69a8f58a9b967c592f965b0a0e4422ee3e04d7
IsaPate <isaakpaterakis@gmail.com>
2026-08-03 14:13:34 +0300

    fix: recurring charges routes

--- src/routes/recurring_charge_routes.ts
+++ src/routes/recurring_charge_routes.ts
@@ -1,10 +1,43 @@
 import { Router } from "express";
+import { isSelfUser, verifyTokenMiddleware } from "../middlewares/auth";
+import { asyncHandler } from "../middlewares/handlers";
+import {
+  getUserRecurringCharges,
+  createUserRecurringCharges,
+  getSingleRecurringCharge,
+} from "../controllers/recurring_charge_controller";
 
 const recurringChargesRouter = Router();
 
-recurringChargesRouter.get("/users/:userId/recurring-charges");
-recurringChargesRouter.post("/users/:userId/recurring-charges");
-recurringChargesRouter.put("/users/:userId/recurring-charges");
-recurringChargesRouter.delete("/users/:userId/recurring-charges");
+recurringChargesRouter.get(
+  "/users/:userId/recurring-charges",
+  verifyTokenMiddleware,
+  isSelfUser,
+  asyncHandler(getUserRecurringCharges)
+);
+recurringChargesRouter.post(
+  "/users/:userId/recurring-charges",
+  verifyTokenMiddleware,
+  isSelfUser,
+  asyncHandler(createUserRecurringCharges)
+);
+
+recurringChargesRouter.get(
+  "/users/:userId/recurring-charges/:recurringChargeId",
+  verifyTokenMiddleware,
+  isSelfUser,
+  asyncHandler(getSingleRecurringCharge)
+);
+
+recurringChargesRouter.put(
+  "/users/:userId/recurring-charges/:recurringChargeId",
+  verifyTokenMiddleware,
+  isSelfUser
+);
+recurringChargesRouter.delete(
+  "/users/:userId/recurring-charges/:recurringChargeId",
+  verifyTokenMiddleware,
+  isSelfUser
+);
 
 export default recurringChargesRouter;
