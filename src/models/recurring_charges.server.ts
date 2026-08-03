34f642b98e211564f03b6281efbf912dc8b2a7f1
IsaPate <isaakpaterakis@gmail.com>
2026-08-03 12:42:48 +0300

    feat: init recurring charges

---
+++ src/models/recurring_charges.server.ts
@@ -0,0 +1,35 @@
+import { prisma } from "../lib/prisma_client";
+
+export const getRecurringChargesByUserId = async (userId: number) => {
+  return await prisma.recurringCharge.findMany({
+    where: {
+      userId,
+    },
+  });
+};
+
+export const getRecurringChargesByUserIdAndByDate = async (
+  userId: number,
+  start: Date,
+  end: Date
+) => {
+  return await prisma.recurringCharge.findMany({
+    where: {
+      userId,
+      createdAt: {
+        lt: end,
+        gt: start,
+      },
+    },
+  });
+};
+
+export const getRecurringChargeByRecurringChargeId = async (
+  recurringChargeId: number
+) => {
+  return await prisma.recurringCharge.findUnique({
+    where: {
+      id: recurringChargeId,
+    },
+  });
+};
