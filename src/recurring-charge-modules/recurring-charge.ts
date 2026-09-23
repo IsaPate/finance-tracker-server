68335c3512ee70c4ce89832ba66ca888a6933f4d
IsaPate <isaakpaterakis@gmail.com>
2026-09-23 14:17:15 +0300

    feat: scan for recurring charges

--- src/recurring-charge-modules/recurring-charge.ts
+++ src/recurring-charge-modules/recurring-charge.ts
@@ -1,4 +1,8 @@
 import { RecurringCharge } from "@prisma/client";
+import {
+  getAllRecurringCharges,
+  updateLastGeneratedAtAndCreateTransaction,
+} from "../models/recurring_charges.server";
 
 export function compareDates(now: Date, start: Date) {
   now.setHours(0, 0, 0, 0);
@@ -6,10 +10,17 @@ export function compareDates(now: Date, start: Date) {
   return now.getTime() === start.getTime();
 }
 
+export function expiredDateOrSame(now: Date, nextChargeDate: Date) {
+  now.setHours(0, 0, 0, 0);
+  nextChargeDate.setHours(0, 0, 0, 0);
+  return now.getTime() >= nextChargeDate.getTime();
+}
+
 export function getUpcomingCharges(recurringCharges: RecurringCharge[]) {
   const now = new Date();
 
   return recurringCharges.map((charge) => {
+    //check from client if now < start cycle?
     const anchor = charge.lastGeneratedAt ?? charge.startCycle;
     const startOrLastGenerated = new Date(anchor);
 
@@ -27,4 +38,26 @@ export function getUpcomingCharges(recurringCharges: RecurringCharge[]) {
         compareDates(now, startOrLastGenerated) || compareDates(now, previous),
     };
   });
+}
+
+export async function scanForRecurringCharges() {
+  const recurring = await getAllRecurringCharges();
+  // perform db operation for each
+
+  for (const upcoming of recurring) {
+    const now = new Date();
+    const anchor = upcoming.lastGeneratedAt ?? upcoming.startCycle;
+
+    const nextCharge = new Date(anchor);
+
+    if (upcoming.frequency === "MONTHLY") {
+      nextCharge.setMonth(nextCharge.getMonth() + 1);
+    } else {
+      nextCharge.setFullYear(nextCharge.getFullYear() + 1);
+    }
+
+    if (expiredDateOrSame(now, nextCharge)) {
+      await updateLastGeneratedAtAndCreateTransaction(nextCharge, upcoming);
+    }
+  }
 }
