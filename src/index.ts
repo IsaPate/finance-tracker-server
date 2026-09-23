a52d4c9e6be5062b4e329a4607c286fd60e119bd
IsaPate <isaakpaterakis@gmail.com>
2026-09-23 14:25:29 +0300

    feat: add cron job

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import cron from "node-cron";
 import { app } from "./app";
 import { logger } from "./lib/logger";
 import { scanForEmailReporting } from "./email-reporting-modules/email-reporting";
+import { scanForRecurringCharges } from "./recurring-charge-modules/recurring-charge";
 
 const port = process.env.PORT || 3000;
 
 cron.schedule("30 10 1 * *", async () => {
   try {
     await scanForEmailReporting();
+  } catch (error) {
+    logger.error(error);
+  }
+});
+
+cron.schedule("* * * * *", async () => {
+  try {
+    await scanForRecurringCharges();
   } catch (error) {
     logger.error(error);
   }
